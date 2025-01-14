a6dc81b3bff5f9ab92257c7f7d8597ecda90c51f
Luv Bhardwaj <bhardwaj.baljit@gmail.com>
2025-01-14 21:43:58 +0530

    created Netflix Sign In Page

---
+++ index.js
@@ -0,0 +1,9 @@
+let input = document.getElementById("second")
+const myfunction  = () =>{
+    if(input.type==="password"){
+        input.type = "text"
+    }
+    else{
+        input.type = "password"
+    }
+}
